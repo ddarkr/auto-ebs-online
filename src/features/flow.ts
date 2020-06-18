e78e891d67bc55120219a8c9628ba284d4202ee2
DDark <ddark.kr@gmail.com>
2020-06-18 16:44:40 +0900

    refactor: make Promise

--- src/features/flow.ts
+++ src/features/flow.ts
@@ -4,19 +4,23 @@ import LoginToSession from './class-login';
 import videoWatch from './video-watch';
 
 const taskFlow = async () => {
-  const session = await InitialPuppeteer();
+  return new Promise((resolve, reject) => {
+    (async () => {
+      const session = await InitialPuppeteer();
 
-  await LoginToSession(session.page);
+      await LoginToSession(session.page);
 
-  await session.page.waitFor(2000);
+      await session.page.waitFor(2000);
 
-  await videoWatch(
-    session.page,
-    'https://hoc19.ebssw.kr/mypage/userlrn/userLrnView.do?atnlcNo=788879&stepSn=154151&lctreSn=5454936&onlineClassYn=Y',
-  );
+      await videoWatch(
+        session.page,
+        'https://hoc19.ebssw.kr/mypage/userlrn/userLrnView.do?atnlcNo=788879&stepSn=154151&lctreSn=5454936&onlineClassYn=Y',
+      );
 
-  // 세션 종료 (개발 시에는 주석 처리해놔)
-  await session.browser.close();
+      // 세션 종료 (개발 시에는 주석 처리해놔)
+      await session.browser.close();
+    })();
+  });
 };
 
 export default taskFlow;
