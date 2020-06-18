8938837ab343412f515656b8d14f22d1d84b967e
DDark <ddark.kr@gmail.com>
2020-06-18 16:52:50 +0900

    feat: videoList 적용

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,14 @@
 
 import checkNeedEnviroment from './core/checkNeedEnviroment';
 import taskFlow from './features/flow';
+
+const videoList: Array<string> = [];
+
 // 환경 변수 존재하는지 확인
 if (checkNeedEnviroment()) {
-  taskFlow();
+  (async () => {
+    videoList.forEach(async (url: string) => {
+      await taskFlow(url);
+    });
+  })();
 }
