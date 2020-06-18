d9fffd210e04145c373007e275a33f356f521baf
DDark <ddark.kr@gmail.com>
2020-06-18 16:40:33 +0900

    feat: 구현은 했는데 문제가 있네

--- src/app.ts
+++ src/app.ts
@@ -36,6 +36,11 @@ if (checkNeedEnviroment()) {
       'https://hoc19.ebssw.kr/mypage/userlrn/userLrnView.do?atnlcNo=788879&stepSn=154151&lctreSn=5454936&onlineClassYn=Y',
     );
 
+    await videoWatch(
+      session.page,
+      'https://hoc19.ebssw.kr/mypage/userlrn/userLrnView.do?atnlcNo=788879&stepSn=154151&lctreSn=5454939&onlineClassYn=Y',
+    );
+
     // 세션 종료 (개발 시에는 주석 처리해놔)
     // await session.browser.close();
   })();
