838773c49e60cf7ff96aebdfd3805d9ef4212fa4
DDark <ddark.kr@gmail.com>
2020-06-18 17:01:39 +0900

    feat: list

--- src/app.ts
+++ src/app.ts
@@ -8,13 +8,16 @@
 import checkNeedEnviroment from './core/checkNeedEnviroment';
 import taskFlow from './features/flow';
 
-const videoList: Array<string> = [];
+const videoList: Array<string> = [
+  'https://hoc19.ebssw.kr/mypage/userlrn/userLrnView.do?atnlcNo=788879&stepSn=154151&lctreSn=5454939&onlineClassYn=Y',
+  'https://hoc19.ebssw.kr/mypage/userlrn/userLrnView.do?atnlcNo=780315&stepSn=171164&lctreSn=6075903&onlineClassYn=Y',
+];
 
 // 환경 변수 존재하는지 확인
 if (checkNeedEnviroment()) {
   (async () => {
-    videoList.forEach(async (url: string) => {
-      await taskFlow(url);
-    });
+    for (let i = 0; i < videoList.length; i++) {
+      await taskFlow(videoList[i]);
+    }
   })();
 }
