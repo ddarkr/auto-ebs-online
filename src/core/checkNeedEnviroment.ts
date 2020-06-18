a582c61c6582228b5d3e538910e13fc15ac2604b
DDark <ddark.kr@gmail.com>
2020-06-18 09:40:34 +0900

    feat: checkNeedEnviroment

---
+++ src/core/checkNeedEnviroment.ts
@@ -0,0 +1,14 @@
+/**
+ * 필요한 환경 변수가 모두 존재하는지 확인
+ */
+const checkNeedEnviroment = (): boolean => {
+  if (!process.env.EBS_ID && process.env.EBS_ID === '') {
+    return false;
+  }
+  if (!process.env.EBS_PW && process.env.EBS_PW === '') {
+    return false;
+  }
+  return true;
+};
+
+export default checkNeedEnviroment;
