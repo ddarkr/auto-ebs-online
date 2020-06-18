3833dd443a455c99dd0f9a65b5a037c34da53c19
DDark <ddark.kr@gmail.com>
2020-06-18 16:26:03 +0900

    feat(video-watch): 구현 된 듯... 리팩토링 필요해보임.

--- src/features/video-watch.ts
+++ src/features/video-watch.ts
@@ -1,11 +1,54 @@
 import puppeteer from 'puppeteer';
-const videoWatch = async (page: puppeteer.Page, video: string): Promise<void> => {
-  console.log(video + ' 로 이동합니다.');
-  await page.goto(video, {
-    waitUntil: 'networkidle2',
-  });
 
-  await page.click('#playerEl > .vjs-big-play-button');
+declare const playerObj: any;
+declare global {
+  interface Window {
+    onCustomEvent: any;
+  }
+}
+
+const videoWatch = async (page: puppeteer.Page, video: string): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    console.log('[video-watch] 시작');
+    const action = async () => {
+      console.log(video + ' 로 이동합니다.');
+      await page.goto(video, {
+        waitUntil: 'networkidle2',
+      });
+
+      console.log('[재생] 큐 처리 스크립트 삽입 작업 실행.');
+      await page.exposeFunction('onCustomEvent', e => {
+        console.log('[재생] 일시정지 감지 됨. (resolved)');
+        resolve(); // 일시정지 감지 된 경우에만 resolve
+      });
+
+      await page.evaluate(() => {
+        playerObj.on('pause', function () {
+          window.onCustomEvent({ type: 'pause' });
+          console.log('paused event');
+        });
+      });
+
+      console.log('[재생] 재생에 방해되는 모달 제거 작업 중...');
+      await page.evaluate(sel => {
+        const elements = document.querySelectorAll(sel);
+        for (let i = 0; i < elements.length; i++) {
+          elements[i].parentNode.removeChild(elements[i]);
+        }
+      }, '.vjs-modal-dialog');
+
+      console.log('[재생] 플레이어 컨트롤 활성화 및 옵션 변경 작업 중...');
+      await page.evaluate(() => {
+        playerObj.muted(true); // 뮤트
+        playerObj.playbackRate(1.5); // 1.5x 재생 속도
+        playerObj.controls(true); // 컨트롤 활성화 (모달 뜨면 안보임)
+      });
+
+      console.log('[재생] 재생 버튼 누름 (재생 시작)');
+      await page.click('#playerEl > .vjs-big-play-button');
+    };
+    action(); // 상단 스크립트 실행.
+  });
 };
 
 export default videoWatch;
