acb3d90e699ffd6ee5eba2cd51531e0e3896a0a8
DDark <ddark.kr@gmail.com>
2020-06-18 10:16:46 +0900

    feat: 생각 수정, scode 추가

--- src/core/checkNeedEnviroment.ts
+++ src/core/checkNeedEnviroment.ts
@@ -2,10 +2,13 @@
  * 필요한 환경 변수가 모두 존재하는지 확인
  */
 const checkNeedEnviroment = (): boolean => {
-  if (!process.env.EBS_ID && process.env.EBS_ID === '') {
+  if (!process.env.EBS_SCODE || process.env.SCODE === '') {
     return false;
   }
-  if (!process.env.EBS_PW && process.env.EBS_PW === '') {
+  if (!process.env.EBS_ID || process.env.EBS_ID === '') {
+    return false;
+  }
+  if (!process.env.EBS_PW || process.env.EBS_PW === '') {
     return false;
   }
   return true;
