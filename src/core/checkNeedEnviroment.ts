9f4cbb26c5768490326fbe7075f43906d22bad16
DDark <ddark.kr@gmail.com>
2020-06-18 10:17:05 +0900

    fix: ebs_

--- src/core/checkNeedEnviroment.ts
+++ src/core/checkNeedEnviroment.ts
@@ -2,7 +2,7 @@
  * 필요한 환경 변수가 모두 존재하는지 확인
  */
 const checkNeedEnviroment = (): boolean => {
-  if (!process.env.EBS_SCODE || process.env.SCODE === '') {
+  if (!process.env.EBS_SCODE || process.env.EBS_SCODE === '') {
     return false;
   }
   if (!process.env.EBS_ID || process.env.EBS_ID === '') {
