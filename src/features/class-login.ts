c2ab2fd9dba26879f6093e5051527e2a0cf8940e
DDark <ddark.kr@gmail.com>
2020-06-18 10:24:38 +0900

    feat: dialog (alert) 자동 수락

--- src/features/class-login.ts
+++ src/features/class-login.ts
@@ -14,6 +14,10 @@ const LoginToSession = async (page: puppeteer.Page): Promise<void> => {
     await page.type('#j_password', process.env.EBS_PW || '');
 
     await page.click('#loginViewForm > div > div.left > fieldset > div > button');
+
+    page.on('dialog', async dialog => {
+      await dialog.accept();
+    });
   } catch (e) {
     console.log('===> 문제 발생!!: feature/LoginToSession');
     console.log('===> 로그인 처리 과정 중 문제가 발생했습니다.');
