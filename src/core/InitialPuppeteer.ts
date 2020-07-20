dd8630812e2614f91824bf1644fc89cdff7d6d25
DDark <ddark.kr@gmail.com>
2020-07-20 09:04:03 +0900

    feat: 이거 왜 작동안하는지 모르겠넴 ㅋㅋ...

--- src/core/InitialPuppeteer.ts
+++ src/core/InitialPuppeteer.ts
@@ -13,8 +13,7 @@ interface InitialType {
 const InitialPuppeteer = async (): Promise<InitialType> => {
   const browser = await puppeteer.launch({
     headless: false,
-    executablePath:
-      config('browser_path') || 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe',
+    executablePath: config('browser_path'),
   });
   const page = await browser.newPage();
   await page.setViewport({ width: 1366, height: 768 }); // 조금 더 큰 화면에서 작업하자
